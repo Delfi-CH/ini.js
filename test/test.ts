4c9c72b5541836e9fcc986098f99e08004604e7e
Delfi-CH <delfi@delfi.dev>
2026-09-09 22:24:34 +0200

    chore: update test formatting

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,14 @@
 import { iniFileToObject } from "../src/public/fs.js";
 
-console.log(await iniToObject("data/example.ini"))
-//await iniToObject("data/incorrect.ini")
+try {
+    console.log("Parsing of valid file was sucessfull:")
+    console.log(await iniFileToObject("data/example.ini"))
+} catch (err) {
+    console.error("Parsing of valid file failed!: " + err)
+}
+
+try {
+    console.error("Parsing of invalid file was sucessfull!:" + await iniFileToObject("data/incorrect.ini"))
+} catch (err) {
+    console.log("Parsing of invalid file failed: "+ err)
+}
